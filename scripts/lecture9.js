ac9d9babb648fee4c0d983d7abc70a4abddecf5d
SelinaKalniske <113166801+SelinaKalniske@users.noreply.github.com>
2022-11-06 11:18:10 +0200

    player

--- scripts/lecture9.js
+++ scripts/lecture9.js
@@ -1,2 +1,19 @@
 // Simple hello world script
+// we use for comments
+// in CSS we use /*....*/
+// in HTML we use <!--.....-->
+// we use the console.log() function to print to the console
 console.log("Hello Javascript!")
+
+
+//finding the first paragraph
+const para = document.querySelector('p');
+
+// add listener to the paragraph
+para.addEventListener('click', updateName);
+
+//defined what to do when the paragraph is clicked
+function updateName() {
+  const name = prompt('Enter a new name');
+  para.textContent = `Player 1: ${name}`;
+}
