c880cc144aa1ad61095dd6f5673ef601216f6b18
SelinaKalniske <113166801+SelinaKalniske@users.noreply.github.com>
2022-11-06 11:38:22 +0200

    changes

--- scripts/lecture9.js
+++ scripts/lecture9.js
@@ -6,14 +6,17 @@
 console.log("Hello Javascript!")
 
 
+//PLAYER EXAMPLE
+
 //finding the first paragraph
 const para = document.querySelector('p');
-
 // add listener to the paragraph
 para.addEventListener('click', updateName);
-
 //defined what to do when the paragraph is clicked
 function updateName() {
   const name = prompt('Enter a new name');
   para.textContent = `Player 1: ${name}`;
+  //we find first element from class results
+  const resultsElement = document.querySelector('#results');
+  resultsElement.textContent= `Updated at ${(new Date()}`;
 }
