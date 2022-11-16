fc2ba08937a61720d12c7aebf98a567e125dd903
SelinaKalniske <113166801+SelinaKalniske@users.noreply.github.com>
2022-11-16 22:31:12 +0200

    comparison

---
+++ scripts/lecture10.js
@@ -0,0 +1,40 @@
+console.log("Hello conditionals and logic!");
+
+let a=10;
+let aString="10";
+//console.log(a==aString);//true, but this is not a good way to compare usually
+console.log(`variable a:${a} and variable aString ${aString} are equal?`, a == aString);
+
+let b=10;
+console.log(`variable a:${a} and variable b ${b} are STRICTLY equal?`, a==b);
+//lets compare a and aString
+
+console.log(`variable a:${a} and variable aString ${aString} are STRICTLY equal?`, a==aString);
+
+console.log(`variable a:${a} and variable aString ${aString} are NOT equal?`, a != aString);//false
+
+let aNumber = Number(aString);
+console.log(`variable a:${a} and variable ${aNumber} are equal?`, a==aNumber);
+
+let anotherString=String(a);
+console.log(`variable a:${aString} and variable ${anotherString} are equal?`, a==anotherString);
+
+let c=42;
+//lets compare a and c using > and < and >=, <=
+
+console.log(`variable a:${a} is greater than variable c ${c}?`, a>c);
+console.log(`variable a:${a} is less than variable c ${c}?`, a<c);
+console.log(`variable a:${a} is greater than or equal to variable c ${c}?`, a>=c);
+console.log(`variable a:${a} is less than or equal to variable c ${c}?`, a<=c);
+
+const myName ="Selina";
+const anotherName ="Solvita";
+console.log(`variable myName${myName} is greater than variable anotherName${anotherName}?`, myName>anotherName);
+console.log(`variable myName${myName} is less than variable anotherName${anotherName}?`, myName<anotherName);
+
+//find ordinal number of a character
+console.log(`ordinal number of a is ${"a".charCodeAt(0)}`);//97
+
+console.log(`ordinal number of ā is ${"ā".charCodeAt(0)}`); //257
+
+
