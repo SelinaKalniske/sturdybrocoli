9a12f7e545d8d22bdd3e1965af136681ee18e9a3
SelinaKalniske <113166801+SelinaKalniske@users.noreply.github.com>
2022-11-22 18:02:10 +0200

    yes

---
+++ scripts/lecture11.js
@@ -0,0 +1,15 @@
+console.log(`looping constructors`);
+
+
+//inifinity loop
+//while(true){
+ //   console.log("Look I am looping");
+//}
+
+let i=0;
+while(i<10){
+    console.log(i);
+    i++;
+}
+confirm.log("After the loop i is " + i);
+confirm.log(`After the loop i is ${i}`);
