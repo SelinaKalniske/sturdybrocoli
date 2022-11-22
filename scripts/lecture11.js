ec3ff7cb2028d11c277cacdc86ac8094c33390f8
SelinaKalniske <113166801+SelinaKalniske@users.noreply.github.com>
2022-11-22 18:16:06 +0200

    do

--- scripts/lecture11.js
+++ scripts/lecture11.js
@@ -15,3 +15,25 @@ while (i>=0){
     i--;
 }
 console.log("After the loop i is " + i);
+console.log(`After the loop i is ${i}`); //dazadi pieraksti
+
+
+//we can use a break statement to exit loop early
+
+i=0;
+while(i<10){
+    console.log(i);
+    if (i==5){
+        console.log(`i is 5 so i am breaking out of the loop`);
+        break;
+    }
+    i++;
+}
+console.log(`After the loop i is ${i}`); 
+
+i=700;
+do{
+    console.log(i);
+    i++;
+} while (i<10);
+console.log(`after do while loop i is ${i}`);
