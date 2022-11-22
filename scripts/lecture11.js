e316cae5f7a0b83a6141a251841a7d0a6e5bcca8
SelinaKalniske <113166801+SelinaKalniske@users.noreply.github.com>
2022-11-22 18:07:23 +0200

    yes

--- scripts/lecture11.js
+++ scripts/lecture11.js
@@ -6,10 +6,18 @@ console.log(`looping constructors`);
  //   console.log("Look I am looping");
 //}
 
-let i=0;
-while(i<10){
+let i = 0;
+while(i < 10){
     console.log(i);
     i++;
 }
-confirm.log("After the loop i is " + i);
-confirm.log(`After the loop i is ${i}`);
+console.log("After the loop i is " + i);
+console.log(`After the loop i is ${i}`); //its same as above
+
+
+i=5;
+while (i>=0){
+    console.log(i);
+    i--;
+}
+console.log("After the loop i is " + i);
