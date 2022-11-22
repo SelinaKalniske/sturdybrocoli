d908b71abbe996607ab31df360baa3fcc446d849
SelinaKalniske <113166801+SelinaKalniske@users.noreply.github.com>
2022-11-22 18:30:23 +0200

    yes

--- scripts/lecture11.js
+++ scripts/lecture11.js
@@ -37,3 +37,40 @@ do{
     i++;
 } while (i<10);
 console.log(`after do while loop i is ${i}`);
+
+i=900;
+while(true){
+    console.log(i);
+    i++;
+    if (i>=10){
+    console.log(`i is 10 or greater so I am breaking out of the loop`);
+    break;
+}
+}
+console.log(`After while loop i is ${i}`);
+
+
+i=0;
+while(i<10){
+    console.log(i)
+    i+=2;
+}
+console.log(`After the loop i is ${i}`);
+
+i=0;
+while(i<=10){
+    console.log(i)
+    i+=2;
+}
+console.log(`After the loop i is ${i}`);
+
+//lets ask for user input
+
+let input=prompt("Enter a number between 1 and 10");
+input=parseInt(input);
+console.log(`After parseInt input i is ${input}`);
+while(input<1|| input>10){
+    input=prompt("iNVALID input. Enter a number between 1 and 10");
+}
+console.log(`You entered ${input}`);
+
