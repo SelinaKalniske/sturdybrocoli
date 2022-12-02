9e6e01a1dff242fd62757279f3696bed5fa37b48
SelinaKalniske <113166801+SelinaKalniske@users.noreply.github.com>
2022-12-02 09:59:50 +0200

    default parametrs

--- scripts/lecture12.js
+++ scripts/lecture12.js
@@ -18,15 +18,19 @@ for(let i=0; i<10; i++){
 function costumHello(name) {
     console.log(`Hello ${name}`);
 }
-//functions can take multiple parametrs
-function costumeHello2(name, age) {
-    console.log(`Hello ${name} you are ${age} years old`)
-}
-
 costumHello('Selina');
 costumHello('Davis');
 costumHello('Kristiana');
 costumHello('Veniamins');
+
+//functions can take multiple parametrs
+function costumHello2(name, age) {
+    console.log(`Hello ${name} you are ${age} years old`)
+}
+costumHello2("Selina", 23);
+costumHello2("Davis", 24);
+costumHello2("Kristiana", 23);
+costumHello2("Veniamins", 24);
 
 //we can use a function to calculate the area of a rectangle
 
@@ -34,6 +38,46 @@ function areaOfRectangle(width,height) {
     let area=width*height;
     console.log(`The area of regtangle with width ${width} and height ${height} is ${area}`);
 }
-
 areaOfRectangle(10,20);
 areaOfRectangle(5,10);
+
+
+// functions can return value
+function add(a,b) {
+    return a+b;//instead of printing return the value to the caller and they can do whatvever they want with it
+}
+let result= add(10,20); //result is global variable
+console.log(`the result of of adding 10 and 20 is ${result}`);
+
+// lets make multiplication function
+function multiply(a,b) {
+    return a*b
+}
+result = multiply(add(10,20), add(30,40)); //result should be 2100
+console.log(`the result of multipling 30 plus 40 and then adding 10 plus 20 is ${result}`);
+
+
+// calling functions within function can be compared to movie Inception where you have a dream within a dream
+//while in a function you can another function and that function call another function and so on
+
+//good practise is to use function witjouth global variables
+
+//function main() {
+   // sayHello();
+ //   costumHello("Selina");
+ //   costumHello2("Selina", 23)
+ //   areaOfRectangle(10,20);
+ //   add(a,b);
+ //   multiply(a*b);    
+//}
+
+//main();
+
+//functions with default parametrs
+function costumHello3(name="Selina", age=24) {
+console.log(`Hello ${name} you are ${age} years old`);    
+}
+costumHello3("Selina", 23)
+costumHello3("Davis")
+
+
