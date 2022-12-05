062e91b070b75a0b4730f70c1b7c8cdf97ffdcc8
SelinaKalniske <113166801+SelinaKalniske@users.noreply.github.com>
2022-12-05 16:32:07 +0200

    yes

--- scripts/lecture13.js
+++ scripts/lecture13.js
@@ -11,3 +11,14 @@ function handleButtonClick() {
 //better to use more specific selector
 const btn=document.querySelector('#myButton'); //will select button with ID myButton
 btn.addEventListener('click', handleButtonClick);
+
+//button 2
+
+const btn2=document.querySelector('#mySecondButton');
+btn2.onclick=handleButtonClick; //shortet way, but only one listener is alloweds
+
+
+//lets handle mouseover event
+function handleMouseOver(){
+    console.log("Mouse is over the button");
+}
