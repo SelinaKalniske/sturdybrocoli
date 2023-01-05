ff5bc028ba1e2416685d3332b274ac55999b5efa
SelinaKalniske <113166801+SelinaKalniske@users.noreply.github.com>
2023-01-05 18:12:43 +0200

    yes

--- scripts/lecture14.js
+++ scripts/lecture14.js
@@ -5,10 +5,31 @@ const container=document.getElementById("container");
 
 //lets make a function that adds new element to container
 function addElement(containerElement) {
+    const parentElement=document.getElementById("container");
     //create a new element
     const newElement= document.createElement("div");
     //add class to new element
     newElement.classList.add("new-element");
-    //add some text to container
-    containerElement.appendChild(newElement);
+    //add new class dpending whether there are an odd or even number elements
+    const numElements=parentElement.childElementCount;
+    if (numElements % 2 == 0){
+        newElement.classList.add("even");
+    } else{
+        newElement.classList.add("odd");
+    }
+      //add some text to container
+    newElement.innerText="Im a new element!";
+    //add element to container
+    parentElement.appendChild(newElement);
 }
+
+
+//lets make main function, because its better to add listener when the page loads
+function main(){
+console.log("main function called");
+const addButton=document.getElementById("addButton")
+addButton.addEventListener("click", addElement);
+}
+ 
+//lets attach main to the window load event
+document.addEventListener("DOMContentLoaded", main);
