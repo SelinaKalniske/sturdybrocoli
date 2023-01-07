224e393652ead1266b3cdf0e10e72f5493953e1f
SelinaKalniske <113166801+SelinaKalniske@users.noreply.github.com>
2023-01-07 10:57:06 +0200

    add 10 elements

--- scripts/lecture14.js
+++ scripts/lecture14.js
@@ -18,9 +18,26 @@ function addElement(containerElement) {
         newElement.classList.add("odd");
     }
       //add some text to container
-    newElement.innerText="Im a new element!";
+      //lets costumize this text depending on number of elements
+      // we will use ` to make template string
+      //we haven't added element so elementCount is off by one
+    newElement.innerText=`I'm element No. ${numElements+1}!`;
+    // lets add a value atribute to the new element
+    newElement.setAttribute("value", numElements+1);//can be used to identify the element
     //add element to container
     parentElement.appendChild(newElement);
+}
+
+//lets make a flexible function to add any number of elements
+function addElement(numElements){
+    //use loop tp add multiple elements
+    for (let i=0; i< numElements; i++){
+        addElement();
+    }
+}
+// function to add 10 elements
+function add10Elements(){
+    addElement(10);
 }
 
 // lets make a function to remove last element from container
@@ -36,14 +53,23 @@ function addElement(containerElement) {
     }
  }
   
-
-//lets make main function, because its better to add listener when the page loads
-function main(){
-console.log("main function called");
+// lets make a function to add listeners
+function addListeners(){
+console.log("Adding listeners")
 const addButton=document.getElementById("addButton")
 addButton.addEventListener("click", addElement);
 const removeButton=document.getElementById("removeButton");
 removeButton.addEventListener("click", removeElement);
+// lets add 10 elements button
+const add10Button=document.getElementById("add10Button");
+add10Button.addEventListener("click", add10Elements);
+}
+
+//lets make main function, because its better to add listener when the page loads
+function main(){
+console.log("main function called");
+//since we have many buttons we could make a separate function to add listeners
+addListeners();
 }
  
 //lets attach main to the window load event
